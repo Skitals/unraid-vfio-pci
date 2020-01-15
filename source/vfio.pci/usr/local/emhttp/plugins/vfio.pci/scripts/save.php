0830cb4805c9ed7ad5470ece13454cd2c8ca3b79
Skitals <justin.t.nolan@gmail.com>
2020-01-15 16:57:47 -0500

    first release

--- source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/save.php
+++ source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/save.php
@@ -10,5 +10,17 @@ if ($cfg) {
 		echo "Return: $myreturn\n";
 		print_r($output);
 	}
+} else {
+	echo "No devices selected.\n\n";
+	echo "Removing vfio-pci.cfg\n\n";
+	exec("rm /boot/config/vfio-pci.cfg", $output, $myreturn );
+	if ($myreturn !== "0") {
+		echo "vfio-pci.cfg removed succesfully.\n\n";
+		echo "You must reboot for changes to take effect!";
+	} else {
+		echo "FAILED\n";
+		echo "Return: $myreturn\n";
+		print_r($output);
+	}
 }
 ?>
