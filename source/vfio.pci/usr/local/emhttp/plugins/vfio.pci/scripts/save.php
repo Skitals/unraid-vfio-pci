38c5165fd003e9053aa76fd9bd33db7f53f8bf4b
Skitals <justin.t.nolan@gmail.com>
2020-01-18 16:11:08 -0500

    added RebootNotification and Undo

--- source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/save.php
+++ source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/save.php
@@ -1,6 +1,13 @@
 <?php
 $cfg = $_GET["cfg"];
+if ($_GET["careboot"] == "no") {
+	$nocareboot = "true";
+}
+if ($nocareboot) {
+	exec("echo \"VFIO-PCI Config\" >/tmp/reboot_notifications");
+}
 if ($cfg) {
+	exec("cp /boot/config/vfio-pci.cfg /boot/config/vfio-pci.cfg.bak");
 	exec("echo \"$cfg\" >/boot/config/vfio-pci.cfg", $output, $myreturn );
 	if ($myreturn !== "0") {
 		echo "vfio-pci.cfg created succesfully.\n\n";
@@ -13,6 +20,7 @@ if ($cfg) {
 } else {
 	echo "No devices selected.\n\n";
 	echo "Removing vfio-pci.cfg\n\n";
+	exec("cp /boot/config/vfio-pci.cfg /boot/config/vfio-pci.cfg.bak");
 	exec("rm /boot/config/vfio-pci.cfg", $output, $myreturn );
 	if ($myreturn !== "0") {
 		echo "vfio-pci.cfg removed succesfully.\n\n";
---
+++ source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/undo.php
@@ -0,0 +1,10 @@
+<?php
+if (is_file("/boot/config/vfio-pci.cfg.bak")) {
+	exec("cp /boot/config/vfio-pci.cfg.bak /boot/config/vfio-pci.cfg");
+} else {
+	exec("rm /boot/config/vfio-pci.cfg");
+}
+if (is_file("/tmp/reboot_notifications")) {
+	exec("sed -i '/VFIO-PCI Config/d' /tmp/reboot_notifications");
+}
+?>
