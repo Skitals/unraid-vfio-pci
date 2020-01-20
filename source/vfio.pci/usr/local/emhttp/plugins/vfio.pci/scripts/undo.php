ed3386050d03a94e5448e9d70320e5e4ce993919
Skitals <justin.t.nolan@gmail.com>
2020-01-20 11:56:06 -0500

    shows devices bound to vfio

--- source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/undo.php
+++ source/vfio.pci/usr/local/emhttp/plugins/vfio.pci/scripts/undo.php
@@ -4,7 +4,9 @@ if (is_file("/boot/config/vfio-pci.cfg.bak")) {
 } else {
 	exec("rm /boot/config/vfio-pci.cfg");
 }
-if (is_file("/tmp/reboot_notifications")) {
-	exec("sed -i '/VFIO-PCI Config/d' /tmp/reboot_notifications");
+if ($_GET["sed"] == "true") {
+	if (is_file("/tmp/reboot_notifications")) {
+		exec("sed -i '/VFIO-PCI Config/d' /tmp/reboot_notifications");
+	}
 }
 ?>
